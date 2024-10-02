d8cf7f95d6caebcdb66b0d61623a9c292bb23b51
Gustavo <srto.mendes@gmail.com>
2024-10-01 22:45:44 -0300

    create delete category method

--- backend/src/main/java/com/mendes/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/mendes/dscatalog/services/CategoryService.java
@@ -3,9 +3,13 @@ package com.mendes.dscatalog.services;
 import com.mendes.dscatalog.dto.CategoryDTO;
 import com.mendes.dscatalog.entities.Category;
 import com.mendes.dscatalog.repositories.CategoryRepository;
-import com.mendes.dscatalog.services.exceptions.EntityNotFoundException;
+import com.mendes.dscatalog.services.exceptions.DatabaseException;
+import com.mendes.dscatalog.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -27,7 +31,7 @@ public class CategoryService {
     @Transactional(readOnly = true)
     public CategoryDTO findById(Long id) {
         Optional<Category> obj = repository.findById(id);
-        Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not Found"));
+        Category entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not Found"));
         return new CategoryDTO(entity);
     }
 
@@ -37,5 +41,29 @@ public class CategoryService {
         entity.setName((dto.getName()));
         entity = repository.save(entity);
         return new CategoryDTO(entity);
+    }
+
+    @Transactional
+    public CategoryDTO update(Long id, CategoryDTO dto) {
+        try{
+            Category entity = repository.getReferenceById(id);
+            entity.setName((dto.getName()));
+            entity = repository.save(entity);
+            return new CategoryDTO(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException("Id not found" + id);
+        }
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    public void delete(Long id) {
+        if(!repository.existsById(id)) {
+            throw new ResourceNotFoundException("Resource not Found with id " + id);
+        }
+        try {
+            repository.deleteById(id);
+        } catch (DataIntegrityViolationException e){
+            throw new DatabaseException("Err integrity violation");
+        }
     }
 }
--- backend/src/main/java/com/mendes/dscatalog/services/exceptions/DatabaseException.java
+++ backend/src/main/java/com/mendes/dscatalog/services/exceptions/DatabaseException.java
@@ -2,11 +2,11 @@ package com.mendes.dscatalog.services.exceptions;
 
 import java.io.Serial;
 
-public class EntityNotFoundException extends  RuntimeException{
+public class DatabaseException extends  RuntimeException{
     @Serial
     private static final long serialVersionUID = 1L;
 
-    public EntityNotFoundException(String msg){
+    public DatabaseException(String msg){
         super(msg);
     }
 }
