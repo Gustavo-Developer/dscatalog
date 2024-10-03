66f253d4d6c5582df10493fb55cd5dd31a88436b
Gustavo <srto.mendes@gmail.com>
2024-10-03 11:54:42 -0300

    refactor pageable

--- backend/src/main/java/com/mendes/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/mendes/dscatalog/resources/CategoryResource.java
@@ -1,19 +1,16 @@
 package com.mendes.dscatalog.resources;
 
 import com.mendes.dscatalog.dto.CategoryDTO;
-import com.mendes.dscatalog.entities.Category;
 import com.mendes.dscatalog.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
-import java.util.ArrayList;
-import java.util.List;
+
 
 @RestController
 @RequestMapping(value = "/categories")
@@ -24,15 +21,11 @@ public class CategoryResource {
 
     @GetMapping
     public ResponseEntity<Page<CategoryDTO>> findAll(
-            @RequestParam(value = "page", defaultValue = "0") Integer page,
-            @RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
-            @RequestParam(value = "orderBy", defaultValue = "name") String orderBy,
-            @RequestParam(value = "direction", defaultValue = "ASC") String direction
+           Pageable pageable
     ) {
 
-        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction),orderBy);
 ;
-        Page<CategoryDTO> list = service.findAllPaged(pageRequest);
+        Page<CategoryDTO> list = service.findAllPaged(pageable);
 
         return ResponseEntity.ok().body(list);
     }
